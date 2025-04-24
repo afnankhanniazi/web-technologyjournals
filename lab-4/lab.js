91be6d19686196b0a9786c07833cd0e0fe327e35
Afnan Abbas Khan <afnanniaxi@gmail.com>
2025-04-24 19:10:49 +0500

    lab-7

---
+++ lab-4/lab.js
@@ -0,0 +1,8 @@
+let student = {
+    name: "Afnan ABBAS KHAN",
+    university: "Bahria University",
+    degree: "BSCS",
+    occupation: "Student"
+};
+
+console.log("Length of object:", Object.keys(student).length);
